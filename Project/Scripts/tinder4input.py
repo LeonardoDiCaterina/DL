125eda4a1b6779a51978e4509c2fe57fb915460c
LeonardoDiCaterina <20240485@novaims.unl.pt>
2025-03-12 17:52:51 +0000

    answers.txt

--- Project/Scripts/tinder4input.py
+++ Project/Scripts/tinder4input.py
@@ -3,21 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+import threading
+import time
 
+import cv2
+import numpy as np
 
 def show_image(image, title):
-    '''
+    """
     Args:
-        image: image file
-        title: title of the image
+        image: Image file (Tensor or NumPy array)
+        title: Title of the image
     
-    Plots the image
-    
-    '''
-    plt.imshow(image)
-    plt.axis('off')
-    plt.title(title)
-    plt.show()
+    Displays the image and waits for a key press.
+    """
+
+    # Convert TensorFlow tensor to NumPy array (if needed)
+    if not isinstance(image, np.ndarray):
+        image = image.numpy()
+
+    # Convert RGB to BGR (OpenCV uses BGR format)
+    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Show the image
+    cv2.imshow(title, image_bgr)
+
+    # Wait for a key press for 7 seconds (7000 ms)
+    cv2.waitKey(2000)
+
+    # Close the image window
+    cv2.destroyAllWindows()
 
 path = '../Data/rare_species 1'
 df = pd.read_csv(path + '/metadata.csv')
@@ -26,33 +41,37 @@ df.head()
 first_image = df['file_path'][0]
 paths_list = df['file_path'].tolist()
 
-answer = []
-
 checkpoint = 0
 # open a file to save the answers
 
-with open('answers.txt', 'w') as file:
+with open("answers.txt", "r", encoding="utf-8") as file:
     #  look for the last line in the file
     for line in file:
         checkpoint += 1
 
 for index, img in enumerate(paths_list):
-    if index < checkpoint:
+    if index < checkpoint - 1:
         continue
-    
-    full_path_image = path + '/' + img
+
+    full_path_image = f"{path}/{img}"
     img_raw = tf.io.read_file(full_path_image)
     img_tensor = tf.image.decode_image(img_raw)
-    show_image(img_tensor, 'Image {}'.format(index))
-    
-    user_said = input('L = like, D = dislike, H = Hard: ')
 
-    # write the answer to the file
+    # Show image in the main thread
+    show_image(img_tensor, f'Image {index}')
+
+    # Take user input AFTER the image is closed
+    user_said = input('L = Like, D = Dislike, H = Hard:\n if tired, type Q\n')
+    
+
+    if user_said.upper() == 'Q':
+        break
+
+    # Write the answer to a file
     with open('answers.txt', 'a') as file:
-        file.write(str(index) + '*' + user_said + '\n')
+        file.write(f"{index}*{user_said}\n")
     
 
-df['answer'] = answer
 
 
 
